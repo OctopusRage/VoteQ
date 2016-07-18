abd0a23c701b64afca220b21dd8359975bd9c4db
djampot1995 <ahyana1995@gmail.com>
2016-07-18 13:31:36 +0700

    add token in body for create vote

--- app/controllers/api/v1/user/votes_controller.rb
+++ app/controllers/api/v1/user/votes_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::User::VotesController < UserController
-	before_action :authenticate_with_token!, only: [:create, :update, :index, :show]
+	before_action :authenticate_with_token!, only: [:update, :index, :show]
 	
 	def index
 		is_current_user = params[:current_user]
@@ -26,15 +26,23 @@ class Api::V1::User::VotesController < UserController
 
 	def create
 		is_open = params[:is_open] 
-		vote = current_user.votes.build(title: params[:title], status: params[:is_open])
-		vote.generate_vote_options(params[:options])
-		if vote.save			
-			render json: {
-				status: 'success', 
-				data: {
-					vote: vote
-				}
-			}, status: 201
+		curr_user = user.where(:token, params[:auth_token]).first
+		if curr_user
+			vote = curr_user.votes.build(title: params[:title], status: params[:is_open])
+			vote.generate_vote_options(params[:options])
+			if vote.save			
+				render json: {
+					status: 'success', 
+					data: {
+						vote: vote
+					}
+				}, status: 201
+			else
+				render json: {
+					status: 'fail', 
+					data: vote.errors
+				}, status: 422
+			end
 		else
 			render json: {
 				status: 'fail', 
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,5 @@
     	begin
       		self.auth_token = Devise.friendly_token
     	end while self.class.exists?(auth_token: auth_token)
-  	end    
+  	end
 end
